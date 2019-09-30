bdba525f12ab85e0b1c7ef3df1bc8e7026037cf9
fengtaob <fengtaob@chanjet.com>
2019-09-30 15:38:56 +0800

    DR-30037 去掉mobile-plugin

--- clientModules.js
+++ clientModules.js
@@ -1,10 +1,5 @@
 // 每个应用模块需要在此注册才能加载
 module.exports = {
-  "mobile-plugin-gzq": cb => {
-    require.ensure([], require => {
-      cb(require("tplus-mobile-plugin-gzq"));
-    },"mobile-plugin-gzq");
-  },
   "mobile-navigator": cb => {
     require.ensure([], require => {
       cb(require("tplus-mobile-navigator"));
