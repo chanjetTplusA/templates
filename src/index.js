df2e8a1c200c698db2fb5f333cd0b0781a557beb
Hzy0913 <hanzhaoy@chanjet.com>
2019-10-15 11:00:15 +0800

    add base template

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'tplusfx';
-
-import './routes';
-import './app.less';
-
-ReactDOM.render(<App />, document.getElementById('root'));
