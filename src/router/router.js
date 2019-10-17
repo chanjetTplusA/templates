f1ac2a100816893e1f8a84e363ff732bc9e6f819
Hzy0913 <hanzhaoy@chanjet.com>
2019-10-17 10:04:06 +0800

    feat: update router component of base template

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-export default Router = ({ component: Component, children, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => (
-      <Component {...props}><Switch>{children}</Switch></Component>
-    )}
-  />
-);
+export default function Router({ component: Component, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Component {...props}><Switch>{children}</Switch></Component>
+      )}
+    />
+  );
+}
