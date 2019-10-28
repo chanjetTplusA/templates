02fc59ca92d767dda724657939ce1f1ad57acafb
Hzy0913 <hanzhaoy@chanjet.com>
2019-10-28 13:44:32 +0800

    feat: repository init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'mutants-microfx';
-
-import './routes';
-import './app.less';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/home/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.less';
-
-export default function Home() {
-  return <div className='hpme-container'>
-    <h1>Hello Mutants ~</h1>
-  </div>
-}
