5f16225403fa5a638478713007aaaad3211c683d
Hzy0913 <hanzhaoy@chanjet.com>
2019-10-31 09:31:59 +0800

    feat: update scripts

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 // 保证加载足够快
 require.ensure([], require => {
   require('babel-polyfill');
-
-  // 把一些常用的第三方包合并在一起加载
   require('react');
   require('react-dom');
   require('react-router');
   require('react-router-dom');
-
   require('mobx');
   require('mobx-react');
-
-  //require('socket.io-client');
-
-  // require('antlr4');
-  // require('nools');
-
   require('i18next-browser-languagedetector');
   require('i18next-xhr-backend');
   require('i18next');
