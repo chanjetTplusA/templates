c61d5796c4417b239eff06dd8756589a20952b06
Hzy0913 <hanzhaoy@chanjet.com>
2020-02-09 17:30:26 +0800

    update ts template

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './style.less';
 
 export default function Home() {
   return (
     <div className="home-container">
+      <div className="center">
+        <div className="logo-box">
+          <div className="logo" />
+        </div>
+      </div>
       <h1>Hello Mutants ~</h1>
     </div>
   );
