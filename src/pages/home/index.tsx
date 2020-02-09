3d3e3958d946d2fde6f45ce1ad772840f569088c
Hzy0913 <hanzhaoy@chanjet.com>
2020-02-09 15:54:01 +0800

    update ts template

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import './style.less';
 
 export default function Home() {
   return (
-    <div className="hpme-container">
+    <div className="home-container">
       <h1>Hello Mutants ~</h1>
     </div>
   );
